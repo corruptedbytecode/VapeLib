850a583ab3612e097d9b765563bdd1d466005b82
vanish <241622833+corruptedbytecode@users.noreply.github.com>
2026-06-10 13:30:26 +0100

    Update new.lua

--- guis/new.lua
+++ guis/new.lua
@@ -2484,7 +2484,7 @@ function mainapi:CreateGUI()
 	makeDraggable(window)
 	local logo = Instance.new('ImageLabel')
 	logo.Name = 'VapeLogo'
-	logo.Size = UDim2.fromOffset(62, 18)
+	logo.Size = UDim2.fromOffset(120, 30)
 	logo.Position = UDim2.fromOffset(11, 10)
 	logo.BackgroundTransparency = 1
 	logo.Image = getcustomasset('newvape/assets/new/threeq.png')
